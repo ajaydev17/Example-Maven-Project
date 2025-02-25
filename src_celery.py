b7c5d108195df1985c438a69d46e1fba58147b8b
uid_017 <53190174+ajaydev17@users.noreply.github.com>
2025-02-25 15:07:57 +0530

    Create src_celery.py

---
+++ src_celery.py
@@ -0,0 +1,17 @@
+from celery.schedules import crontab
+from celery import Celery
+
+# create a celery client which connects to redis server
+c_app = Celery("celery")
+c_app.config_from_object("src.config")
+
+# celery beat config
+c_app.conf.beat_schedule = {
+    "check-investments-every-5-mins": {
+        "task": "src.celery_tasks.check_investments",  # Correct task name
+        "schedule": crontab(minute="*/2"),  # Runs every 5 minutes
+    },
+}
+
+# celery timezone
+c_app.conf.timezone = "UTC"
